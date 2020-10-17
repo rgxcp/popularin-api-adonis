6e281b27c41efb41d319621b8285a7de5a9516e8
Rommy Gustiawan <rgcppp@gmail.com>
2020-10-17 21:04:04 +0700

    Membuat kerangka trait

---
+++ app/Models/Traits/StorePoint.js
@@ -0,0 +1,10 @@
+'use strict'
+
+class StorePoint {
+  register(Model, customOptions = {}) {
+    const defaultOptions = {}
+    const options = Object.assign(defaultOptions, customOptions)
+  }
+}
+
+module.exports = StorePoint
